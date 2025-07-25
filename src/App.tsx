225d3ee523c60899c629248c8a4711482463af59
Hoanghieu <shivershady@gmail.com>
2025-07-25 08:39:08 +0700

    refactor: remove unused performance and SEO initialization code

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,11 @@
 import Providers from "@/providers";
 import AppRouter from "@/router/AppRouter";
-import { useEffect } from "react";
-import { initPerformanceOptimizations } from "./utils/performance";
-import { initSEO } from "./utils/seo";
 
 const App = () => {
-  useEffect(() => {
-    initPerformanceOptimizations();
-    initSEO();
-  }, []);
+  // useEffect(() => {
+  //   initPerformanceOptimizations();
+  //   initSEO();
+  // }, []);
 
   return (
     <Providers>
