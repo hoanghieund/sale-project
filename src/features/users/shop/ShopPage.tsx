02d763ab2014849d7fd9b99b27f6e107f56e6f6c
Hoanghieu <shivershady@gmail.com>
2025-08-22 14:02:49 +0700

    style: adjust grid gap spacing in shop product layout

--- src/features/users/shop/ShopPage.tsx
+++ src/features/users/shop/ShopPage.tsx
@@ -470,7 +470,7 @@ const ShopPage = () => {
               <LoadingSpinner />
             ) : (
               <>
-                <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-6 gap-2 sm:gap-6">
+                <div className="grid grid-cols-2 md:grid-cols-3 xl:grid-cols-6 gap-2">
                   {products.map(product => (
                     <ProductCardSimple
                       key={product.id}
